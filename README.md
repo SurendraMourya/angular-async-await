75083116d2c24726dd4e838765d4b542e56d48e7
Surendranath Mourya <10604456@thyssenkrupp.com>
2020-06-08 19:29:38 +0530

    First commit

--- README.md
+++ README.md
@@ -25,3 +25,9 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+
+To run JSON-Server on local machine run command: npx json-server --watch db.json
+
+https://medium.com/@pavankjadda/synchronous-http-calls-in-angular-7-using-async-and-await-d0af7910f6d7
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { Employee } from './model/employee';
+import { EmployeeService } from './service/employee.service';
+export const API_URL='http://localhost:3000/';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-aync-await';
+  name='Angular'
+  employees: Array<Employee>;
+
+    constructor(private employeeService:EmployeeService)
+    {
+
+    }
+
+    ngOnInit(): void
+    {
+        this.getEmployees();
+    }
+
+    getEmployees()
+    {
+        let url=API_URL+'employees';
+        this.employeeService.getEmployees(url).subscribe(
+            data=>
+            {
+                this.employees=data;
+            },
+            error1 =>
+            {
+                console.log('Error');
+            }
+        )
+    }
+
+    employeesAvailable()
+    {
+        return this.employees!=undefined;
+    }
+
+    async createEmployee()
+    {
+        let url = API_URL + 'employees';
+        let employee = new Employee();
+        employee.id = Math.floor(Math.random() * 10000);
+        employee.firstName = "John";
+        employee.lastName = "McCain" + employee.id;
+
+        let createdEmployee=await this.employeeService.createEmployee(url, employee);
+        console.log('Created Employee: '+createdEmployee);
+        this.getEmployees();
+    }
 }
